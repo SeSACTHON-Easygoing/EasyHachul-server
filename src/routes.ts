426b5d923c3f10299b3861d6baffc4ae093225a9
tastekim <tastekim@kakao.com>
2023-06-14 23:20:13 +0900

    :rocket: v1.5.5

--- src/routes.ts
+++ src/routes.ts
@@ -6,4 +6,4 @@ const router = new Router();
 export const allRouter = router
   .get('전체 지하철역 정보 가져오기', '/', getStationInfoCtr)
   .get('역 이름 검색하기', '/search', searchStationNameCtr)
-  .post('지하철 경로 정보 가져오기', '/test', getRouteInfoCtr);
+  .post('지하철 경로 정보 가져오기', '/route', getRouteInfoCtr);
